2a45b8808e446d3a5be65665e5ed4a97f39644c9
Chris Jones <gwyn20@gmail.com>
2017-10-22 16:20:37 -0400

    blocitoff-ConfigureFirebase: In Progress

---
+++ app/scripts/factories/Task.js
@@ -0,0 +1,18 @@
+(function() {
+    function Task($firebaseArray) {
+        var Task = {};
+        var ref = firebase.database().ref().child('tasks');
+        var tasks = $firebaseArray(ref);
+    
+        return {
+            all: tasks,
+            add: function(newTask) {
+                rooms.$add({ name: newTask }); 
+            }
+        }
+    };
+    
+    angular
+    .module('blocitoff')
+    .factory('Task', ['$firebaseArray', Task]);
+})();
